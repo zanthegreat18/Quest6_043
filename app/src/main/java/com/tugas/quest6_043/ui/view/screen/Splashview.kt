bf01819c852f046d38fa01be07a0c26676045981
ZanWhoAsk? <sulistiyofauzan27@gmail.com>
2024-11-22 17:01:38 +0700

    Penambahan File Splashview 2

--- app/src/main/java/com/tugas/quest6_043/ui/view/screen/Splashview.kt
+++ app/src/main/java/com/tugas/quest6_043/ui/view/screen/Splashview.kt
@@ -1,12 +1,21 @@
 package com.tugas.quest6_043.ui.view.screen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 
 fun Splashview(
     onMulaiButton: () -> Unit,
@@ -22,3 +31,23 @@ fun Splashview(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Image(
+            painter = painterResource(
+                id = R.drawable.umy
+            ),
+            contentDescription = "",
+            modifier = Modifier.size(150.dp)
+        )
+        Spacer(modifier = Modifier.padding(16.dp))
+        Button(
+            onClick = {
+                onMulaiButton()
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(32.dp),
+        ){
+            Text(text = "Mulai")
+        }
+    }
+}
