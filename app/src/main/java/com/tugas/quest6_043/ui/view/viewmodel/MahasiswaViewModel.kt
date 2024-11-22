90b445df2588b67f20fd614d6392483d4b35f2dc
ZanWhoAsk? <sulistiyofauzan27@gmail.com>
2024-11-22 17:05:10 +0700

    Penambahan File untuk view model isian

--- app/src/main/java/com/tugas/quest6_043/ui/view/viewmodel/MahasiswaViewModel.kt
+++ app/src/main/java/com/tugas/quest6_043/ui/view/viewmodel/MahasiswaViewModel.kt
@@ -1,9 +1,25 @@
 package com.tugas.quest6_043.ui.view.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.tugas.quest6_043.model.Mahasiswa
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 class MahasiswaViewModel : ViewModel() {
     private val _dataModel = MutableStateFlow(Mahasiswa())
     val dataModel: StateFlow<Mahasiswa> = _dataModel.asStateFlow()
+
+    fun saveDataMhs(
+        listData : MutableList<String>
+    ){
+        _dataModel.update { mhs ->
+            mhs.copy(
+                nim = listData[0],
+                nama = listData[1],
+                email = listData[2]
+            )
+        }
+    }
+}
