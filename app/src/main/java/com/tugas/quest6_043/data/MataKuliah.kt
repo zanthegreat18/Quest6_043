21a903a602602265b02230924ac448d7b23a076d
ZanWhoAsk? <sulistiyofauzan27@gmail.com>
2024-11-22 16:48:16 +0700

    Penambahan data matakuliah

---
+++ app/src/main/java/com/tugas/quest6_043/data/MataKuliah.kt
@@ -0,0 +1,12 @@
+package com.tugas.quest6_043.data
+
+object MataKuliah {
+    val option = listOf(
+        "Pemrograman Aplikasi Mobile Lanjut",
+        "Pengembangan Aplikasi Web Framework",
+        "Prototyping",
+        "Aplikasi Multimedia",
+        "Kecerdasan Buatan",
+        "Computer Vission"
+    )
+}
